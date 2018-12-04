e3dd1a579ae0205fba5319dbdae193602234c403
Andrii Dubytskyi <andrii.dubytskyi@gmail.com>
2018-12-04 13:12:07 -0600

    Initial commit

---
+++ Spring/coupling/src/main/java/edu/javafullstack/springmaster/coupling/CouplingApplication.java
@@ -0,0 +1,12 @@
+package edu.javafullstack.springmaster.coupling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouplingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouplingApplication.class, args);
+	}
+}
---
+++ Spring/coupling/src/test/java/edu/javafullstack/springmaster/coupling/CouplingApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.javafullstack.springmaster.coupling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CouplingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
